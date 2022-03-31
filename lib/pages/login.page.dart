57d80dcea9ad73c8611fda0c235bb075001c8f5c
MiltonQuirino <miltonquirino.br@gmail.com>
2022-03-31 08:18:26 -0300

    add email and password

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -17,45 +17,76 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Container(
-              height: 450,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    offset: new Offset(3, 2.0),
-                    blurRadius: 5,
-                    spreadRadius: 1,
-                  )
-                ],
-              ),
-              child: ListView(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                height: 450,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: new Offset(3, 2.0),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: ListView(
                     children: <Widget>[
-                      Column(
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          Text(
-                            "Welcome",
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w500,
-                            ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                "Welcome",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text("Sign in to continue"),
+                            ],
                           ),
-                          Text("Sign in to continue"),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text("Sign Up"),
+                          )
                         ],
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text("Sign Up"),
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16),
+                        ),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16),
+                        ),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
                       )
                     ],
-                  )
-                ],
-              ),
-            )
+                  ),
+                ))
           ],
         ),
       ),
