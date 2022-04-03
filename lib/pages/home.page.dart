0d2a9212e218f617a4ebfb92153cb83085e8b4ef
MiltonQuirino <miltonquirino.br@gmail.com>
2022-04-03 08:17:29 -0300

    Layout home page

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -17,6 +17,47 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             search(),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("See All"),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 350,
+              child: productList(),
+            )
           ],
           // child: Text("Home Page"),
         ),
@@ -60,5 +101,128 @@ Widget search() {
 
       // Text("Search"),
     ]),
+  );
+}
+
+Widget categoryList() {
+  return Container(
+    width: 70,
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+    width: 70,
+    height: 70,
+    margin: EdgeInsets.all(10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      boxShadow: [
+        new BoxShadow(
+          color: Colors.black12,
+          offset: new Offset(1, 1),
+          blurRadius: 5,
+          spreadRadius: 2,
+        )
+      ],
+      borderRadius: BorderRadius.all(
+        Radius.circular(64),
+      ),
+    ),
+    child: Image.asset("assets/Icon_Devices.png"),
+  );
+}
+
+Widget productList() {
+  return Container(
+    width: 70,
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+      ],
+    ),
+  );
+}
+
+Widget productItem() {
+  return Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: 170,
+    color: Colors.black12,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          "assets/product-1.png",
+          width: 170,
+          height: 170,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          child: Text(
+            "Titulo do produto",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Marca",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "\$300",
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF00C569)),
+        ),
+      ],
+    ),
   );
 }
