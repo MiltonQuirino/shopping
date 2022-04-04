c153c79083201285248feda23cb710fc17e838d1
MiltonQuirino <miltonquirino.br@gmail.com>
2022-04-03 21:57:24 -0300

    create widgets

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/pages/product.page.dart';
+import 'package:shopping/widgets/category/category-item.widget.dart';
+import 'package:shopping/widgets/category/category-list.widget.dart';
+import 'package:shopping/widgets/search-box.widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,7 +20,7 @@ class HomePage extends StatelessWidget {
             SizedBox(
               height: 60,
             ),
-            search(),
+            SearchBox(),
             SizedBox(
               height: 30,
             ),
@@ -32,7 +35,7 @@ class HomePage extends StatelessWidget {
             ),
             Container(
               height: 90,
-              child: categoryList(),
+              child: CategoryList(),
             ),
             SizedBox(
               height: 30,
@@ -65,88 +68,6 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget search() {
-  return Container(
-    height: 60,
-    padding: EdgeInsets.only(left: 20),
-    decoration: BoxDecoration(
-      color: Colors.black.withOpacity(0.1),
-      borderRadius: BorderRadius.all(
-        Radius.circular(128),
-      ),
-    ),
-    child: Row(children: [
-      Icon(Icons.search),
-      Container(
-        width: 300,
-        padding: EdgeInsets.only(left: 10),
-        child: TextFormField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            labelText: "Search ...",
-            labelStyle: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.w300,
-              fontSize: 16,
-            ),
-          ),
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-          ),
-        ),
-      )
-
-      // Text("Search"),
-    ]),
-  );
-}
-
-Widget categoryList() {
-  return Container(
-    width: 70,
-    child: ListView(
-      scrollDirection: Axis.horizontal,
-      children: [
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-      ],
-    ),
-  );
-}
-
-Widget categoryItem() {
-  return Container(
-    width: 70,
-    height: 70,
-    margin: EdgeInsets.all(10),
-    padding: EdgeInsets.all(10),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      boxShadow: [
-        new BoxShadow(
-          color: Colors.black12,
-          offset: new Offset(1, 1),
-          blurRadius: 5,
-          spreadRadius: 2,
-        )
-      ],
-      borderRadius: BorderRadius.all(
-        Radius.circular(64),
-      ),
-    ),
-    child: Image.asset("assets/Icon_Devices.png"),
-  );
 }
 
 Widget productList(BuildContext context) {
---
+++ lib/widgets/search-box.widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      padding: EdgeInsets.only(left: 20),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.1),
+        borderRadius: BorderRadius.all(
+          Radius.circular(128),
+        ),
+      ),
+      child: Row(children: [
+        Icon(Icons.search),
+        Container(
+          width: 300,
+          padding: EdgeInsets.only(left: 10),
+          child: TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              labelText: "Search ...",
+              labelStyle: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.w300,
+                fontSize: 16,
+              ),
+            ),
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+          ),
+        )
+
+        // Text("Search"),
+      ]),
+    );
+  }
+}
