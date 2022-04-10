708b7db06296fcf7a3120fd66bf80a44d5b1efe0
MiltonQuirino <miltonquirino.br@gmail.com>
2022-04-10 16:05:42 -0300

    add animated search

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shopping/widgets/category/category-list.widget.dart';
 import 'package:shopping/widgets/product/product-list.widget.dart';
 import 'package:shopping/widgets/search-box.widget.dart';
-
 import '../widgets/banner/banner.widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,63 +13,66 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(15),
-        width: double.infinity,
-        // color: Color(0xFFF5F5F5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            SearchBox(),
-            SizedBox(
-              height: 10,
-            ),
-            BannerHome(),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Categories",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 70,
-              child: CategoryList(),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Best Selling",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("See All"),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 300,
-              child: ProductList(
-                scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.all(15),
+          width: double.infinity,
+          // color: Color(0xFFF5F5F5),
+          child: Wrap(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 60,
+                  ),
+                  SearchBox(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  BannerHome(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Categories",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 70,
+                    child: CategoryList(),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Best Selling",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("See All"),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 300,
+                    child: ProductList(
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  )
+                ],
+                // child: Text("Home Page"),
               ),
-            )
-          ],
-          // child: Text("Home Page"),
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
--- lib/widgets/search-box.widget.dart
+++ lib/widgets/search-box.widget.dart
@@ -1,44 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/widgets/animated-input.widget.dart';
 
-class SearchBox extends StatelessWidget {
+class SearchBox extends StatefulWidget {
   const SearchBox({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      padding: EdgeInsets.only(left: 20),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.1),
-        borderRadius: BorderRadius.all(
-          Radius.circular(128),
-        ),
-      ),
-      child: Row(children: [
-        Icon(Icons.search),
-        Container(
-          width: 200,
-          padding: EdgeInsets.only(left: 10),
-          child: TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              labelText: "Search ...",
-              labelStyle: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.w300,
-                fontSize: 16,
-              ),
-            ),
-            style: TextStyle(
-              fontSize: 20,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-        )
+  State<SearchBox> createState() => _SearchBoxState();
+}
 
-        // Text("Search"),
-      ]),
+class _SearchBoxState extends State<SearchBox>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool menuOpened = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 1,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // Text("search"),
+        AnimetedInput(controller: _controller),
+        TextButton(
+          onPressed: () {
+            !menuOpened ? _controller.forward() : _controller.reverse();
+            menuOpened = !menuOpened;
+          },
+          child: AnimatedIcon(
+            icon: AnimatedIcons.search_ellipsis,
+            progress: _controller,
+            semanticLabel: 'Open Menu',
+          ),
+        ),
+      ],
     );
   }
 }
+
+
+// Container(
+//       height: 40,
+//       padding: EdgeInsets.only(
+//         left: 20,
+//       ),
+//       decoration: BoxDecoration(
+//         color: Colors.black.withOpacity(0.1),
+//         borderRadius: BorderRadius.all(
+//           Radius.circular(128),
+//         ),
+//       ),
+//       child: Row(children: [
+//         Icon(Icons.search),
+//         Container(
+//           width: 200,
+//           padding: EdgeInsets.only(left: 10),
+//           child: TextFormField(
+//             keyboardType: TextInputType.text,
+//             decoration: InputDecoration(
+//               border: InputBorder.none,
+//               labelText: "Search ...",
+//               labelStyle: TextStyle(
+//                 color: Theme.of(context).colorScheme.primary,
+//                 fontWeight: FontWeight.w300,
+//                 fontSize: 16,
+//               ),
+//             ),
+//             style: TextStyle(
+//               fontSize: 20,
+//               color: Theme.of(context).colorScheme.primary,
+//             ),
+//           ),
+//         )
+
+//         // Text("Search"),
+//       ]),
+//     );
